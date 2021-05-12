f5dfe42ea842afeaa9a17df55bfe389e118694f1
jefferfonseca <63508641+jefferfonseca@users.noreply.github.com>
2021-05-12 18:34:36 -0500

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,11 @@
+let x = 4
+let y = 0
+for (let index = 0; index < 5; index++) {
+    for (let index = 0; index < 6; index++) {
+        y = x + y
+    }
+}
+basic.showNumber(y)
+basic.forever(function () {
+	
+})
